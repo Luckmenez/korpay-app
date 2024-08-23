07526be6b2eea14715da381e0107beb47d0a2d13
Lucas Menezes <luckmenez@gmail.com>
2024-08-22 22:43:41 -0300

    feat: implementado layout para páginas

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -4,28 +4,31 @@ import { Login } from '../pages/Login';
 import { Quotation } from '../pages/Quotation';
 import { Admin } from '../pages/Admin';
 import { ProtectedRoutes } from './ProtectedRoutes';
+import { StandardLayout } from '../layout/StandardLayout';
 
 export function Router() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                <Route path="/" element={<Login />} />
-                <Route
-                    path="/quotation"
-                    element={
-                        <ProtectedRoutes requiredRole="USER">
-                            <Quotation />
-                        </ProtectedRoutes>
-                    }
-                />
-                <Route
-                    path="/admin"
-                    element={
-                        <ProtectedRoutes requiredRole="ADMIN">
-                            <Admin />
-                        </ProtectedRoutes>
-                    }
-                />
+                <Route element={<StandardLayout />}>
+                    <Route path="/" element={<Login />} />
+                    <Route
+                        path="/quotation"
+                        element={
+                            <ProtectedRoutes requiredRole="USER">
+                                <Quotation />
+                            </ProtectedRoutes>
+                        }
+                    />
+                    <Route
+                        path="/admin"
+                        element={
+                            <ProtectedRoutes requiredRole="ADMIN">
+                                <Admin />
+                            </ProtectedRoutes>
+                        }
+                    />
+                </Route>
             </Routes>
         </Suspense>
     );
