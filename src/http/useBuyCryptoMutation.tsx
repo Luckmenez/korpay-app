be2fa06d6f89b761fff17ec9a5b2889d7739a54c
Lucas Menezes <luckmenez@gmail.com>
2024-11-04 18:49:21 -0300

    feat: removido chamada do whatsapp

--- src/http/useBuyCryptoMutation.tsx
+++ src/http/useBuyCryptoMutation.tsx
@@ -39,15 +39,6 @@ export const useBuyCryptoMutation = () => {
         onSuccess: (data) => {
             if (data.status === 201) {
                 console.log(data.data);
-                const message = `Olá, gostaria de solicitar a seguinte compra:\nUSDT: ${data.data.fiat_value}\nValor: ${
-                    data.data.crypto_value
-                }\nID da Cotação: ${data.data.order_id}`;
-
-                const encodedMessage = encodeURIComponent(message);
-
-                const whatsappUrl = `https://wa.me/${import.meta.env.VITE_WHATSAPP_NUMBER}?text=${encodedMessage}`;
-
-                window.open(whatsappUrl, '_blank');
             }
         },
         onError: (error: AxiosError<ApiErrorResponse>) => {
